ce033ba062f02e9ad68d68d7da21d72409f905f7
uchitsa <uchitsa@ya.ru>
2024-07-09 19:35:28 +0300

    update check-dates.py

--- .github/scripts/check-dates.py
+++ .github/scripts/check-dates.py
@@ -11,3 +11,6 @@ gh = Github(token)
 repo = gh.get_repo(os.environ.get('GITHUB_REPOSITORY'))
 readme = repo.get_readme()
 readme_content = readme.decoded_content.decode('utf-8')
+
+date_pattern = re.compile(
+    r"^[0-9]{4}-(((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))$")
