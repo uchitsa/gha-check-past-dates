ee1e06b43c444f4293400748857463cd66873c7d
uchitsa <uchitsa@ya.ru>
2024-07-09 19:36:00 +0300

    update check-dates.py

--- .github/scripts/check-dates.py
+++ .github/scripts/check-dates.py
@@ -14,3 +14,7 @@ readme_content = readme.decoded_content.decode('utf-8')
 
 date_pattern = re.compile(
     r"^[0-9]{4}-(((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))$")
+
+
+def date_in_past(date):
+    return datetime.strptime(date, "%Y-%m-%d") < datetime.now()
