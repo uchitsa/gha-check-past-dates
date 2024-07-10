b401526c6c78a52add7e4aab892d2ad9302aaaea
uchitsa <uchitsa@ya.ru>
2024-07-11 02:29:53 +0300

    update README.md

--- .github/scripts/check_dates.py
+++ .github/scripts/check_dates.py
@@ -3,9 +3,9 @@ from github import Github
 import re
 from datetime import datetime
 
-token = os.environ.get('GITHUB_TOKEN')
+token = os.environ.get('TOKEN')
 if token is None:
-    raise ValueError('GITHUB_TOKEN env is not set')
+    raise ValueError('TOKEN env is not set')
 
 gh = Github(token)
 repo = gh.get_repo(os.environ.get('GITHUB_REPOSITORY'))
