05c47dfb64920c12efb19c3671052f12f5cd899d
uchitsa <uchitsa@ya.ru>
2024-07-11 02:23:46 +0300

    update README.md

--- .github/scripts/check_dates.py
+++ .github/scripts/check_dates.py
@@ -12,8 +12,7 @@ repo = gh.get_repo(os.environ.get('GITHUB_REPOSITORY'))
 readme = repo.get_readme()
 readme_content = readme.decoded_content.decode('utf-8')
 
-date_pattern = re.compile(
-    r"^[0-9]{4}-(((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))$")
+date_pattern = re.compile(r"\b\d{4}-\d{2}-\d{2}\b")
 
 
 def date_in_past(date):
