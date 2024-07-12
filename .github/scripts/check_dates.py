01c34956b3656403bb091b8e58089477eaf33ca5
uchitsa <uchitsa@ya.ru>
2024-07-12 14:19:13 +0300

    update check_dates.py

--- .github/scripts/check_dates.py
+++ .github/scripts/check_dates.py
@@ -9,7 +9,9 @@ repo = gh.get_repo("https://api.github.com/repos/uchitsa/gha-check-past-dates")
 readme = repo.get_readme()
 readme_content = readme.decoded_content.decode('utf-8')
 
-date_pattern = re.compile(r"\b\d{4}-\d{2}-\d{2}\b")
+date_pattern = re.compile(
+    r"^[0-9]{4}-(((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))$"
+)
 
 
 def date_in_past(date):
